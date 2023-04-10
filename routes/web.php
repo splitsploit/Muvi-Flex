91e08237dbaee105e05baf244c6b55c73a106b0a
unknown <bayuxbca@gmail.com>
2023-04-10 21:58:15 +0700

    index website Muvi-Flex

--- routes/web.php
+++ routes/web.php
@@ -6,20 +6,9 @@ use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::view('/', 'index');
 
 // ! test route
 // Route::get('test', function() {
