100beaf9ac303c03280a91c9fe3b74f2bff1c5a4
unknown <bayuxbca@gmail.com>
2023-04-20 14:55:11 +0700

    make validation, if user has buy package can watch movie

--- app/Http/Controllers/Member/Moviecontroller.php
+++ app/Http/Controllers/Member/Moviecontroller.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Member;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\UserPremium;
+use Illuminate\Support\Facades\Auth;
 
 class Moviecontroller extends Controller
 {
@@ -12,6 +14,16 @@ class Moviecontroller extends Controller
     }
 
     public function watch($id) {
-        return view('member.movie-watching');
+        $userId = Auth::user()->id;
+
+        // dd($userId);
+        
+        $userPremium = UserPremium::where('user_id', $userId)->first();
+
+        if($userPremium) {
+            return view('member.movie-watching');
+        }
+
+        return redirect()->route('member.pricing');
     }
 }
