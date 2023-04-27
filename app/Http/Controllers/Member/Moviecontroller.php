0825773492021f9d4234159694e286e8b6839d28
unknown <bayuxbca@gmail.com>
2023-04-27 21:49:34 +0700

    validation, check if member has end of subscription, cannot watch movie

--- app/Http/Controllers/Member/Moviecontroller.php
+++ app/Http/Controllers/Member/Moviecontroller.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers\Member;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use App\Models\UserPremium;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class Moviecontroller extends Controller
@@ -21,9 +22,18 @@ class Moviecontroller extends Controller
         $userPremium = UserPremium::where('user_id', $userId)->first();
 
         if($userPremium) {
-            return view('member.movie-watching');
-        }
 
-        return redirect()->route('member.pricing');
+            $endOfSubscription = $userPremium->end_of_subscription;
+
+            $date = Carbon::createFromFormat('Y-m-d', $endOfSubscription);
+
+            $isValidSubscription = $date->greaterThan(now());
+            // dd($isValidSubscription);
+
+            if($isValidSubscription) {
+                return view('member.movie-watching');
+            }
+            return redirect()->route('member.pricing');  
+        }
     }
 }
