54d4dea5390f38b096483cd1cd4555dd101a5756
unknown <bayuxbca@gmail.com>
2023-05-05 22:27:09 +0700

    use eloquent instead manual define at webhook controller

--- app/Http/Controllers/Member/WebHookController.php
+++ app/Http/Controllers/Member/WebHookController.php
@@ -64,8 +64,8 @@ class WebHookController extends Controller
           
         }
 
-        $transaction = Transaction::where('transaction_code', $orderId)->first();
-        $package = Package::find($transaction->package_id);
+        $transaction = Transaction::with('package')->where('transaction_code', $orderId)->first();
+        // $package = Package::find($transaction->package_id); // old ( unused )
 
         if($status === 'success') {
 
@@ -78,7 +78,7 @@ class WebHookController extends Controller
                 // renewal subscription
                 $endOfSubscription = $userPremium->end_of_subscription;
                 $date = Carbon::createFromFormat('Y-m-d', $endOfSubscription); // change format data from string to date carbon
-                $newEndOfSubscription = $date->addDays($package->max_days)->format('Y-m-d'); // add new end subscription & change format to string again
+                $newEndOfSubscription = $date->addDays($transaction->package->max_days)->format('Y-m-d'); // add new end subscription & change format to string again
 
                 // dd($newEndOfSubscription);
 
@@ -91,9 +91,9 @@ class WebHookController extends Controller
 
                 // if user premium data not exsist, create new subscription
                 UserPremium::create([
-                    'package_id' => $package->id,
+                    'package_id' => $transaction->package->id,
                     'user_id' => $transaction->user_id,
-                    'end_of_subscription' => Carbon::now()->addDays($package->max_days)
+                    'end_of_subscription' => Carbon::now()->addDays($transaction->package->max_days)
                 ]);
             }
 
