3340697381226a2bc86f1059273283601893174c
unknown <bayuxbca@gmail.com>
2023-05-08 19:57:05 +0700

    show featured movie from database to member dashboard

--- app/Http/Controllers/Member/DashboardController.php
+++ app/Http/Controllers/Member/DashboardController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers\Member;
 
-use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return view('member.dashboard');
+
+        $movies = Movie::orderBy('featured', 'DESC')->orderBy('created_at', 'DESC')->get();
+
+        return view('member.dashboard', compact('movies'));
     }
 }
