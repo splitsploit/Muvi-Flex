ec3c8ef8b0a6ed491405540a1d90304988bb8d53
unknown <bayuxbca@gmail.com>
2023-05-15 21:54:28 +0700

    adding detail comment at movie controller ( member )

--- app/Http/Controllers/Member/Moviecontroller.php
+++ app/Http/Controllers/Member/Moviecontroller.php
@@ -13,33 +13,43 @@ class Moviecontroller extends Controller
 {
     public function show($id) {
 
+        // find / get movie by id
         $movie = Movie::find($id);
 
         return view('member.movie-detail', ['movie' => $movie]);
     }
 
     public function watch($id) {
+        
+        // get user id value
         $userId = Auth::user()->id;
 
         // dd($userId);
-        
+
+        // user premium check
         $userPremium = UserPremium::where('user_id', $userId)->first();
 
+        // if user premium is true
         if($userPremium) {
 
+            // get end of subscription value
             $endOfSubscription = $userPremium->end_of_subscription;
 
+            // make end of subscription to be a date format. Not an string
             $date = Carbon::createFromFormat('Y-m-d', $endOfSubscription);
 
+            // valid subscription check
             $isValidSubscription = $date->greaterThan(now());
-            // dd($isValidSubscription);
 
+            // if valid subscription is true
             if($isValidSubscription) {
 
                 $movie = Movie::find($id);
 
                 return view('member.movie-watching', ['movie' => $movie]);
             }
+
+            // if valid subscription is false
             return redirect()->route('member.pricing')
             ->withErrors([
                 'expired' => 'Maaf, Subscription Anda Sudah Habis. Silahkan Berlangganan Kembali'
