5cfe0af2b37eeafe683249a39909e8c13715d383
unknown <bayuxbca@gmail.com>
2023-05-16 19:57:34 +0700

    adding detail comment at auth API controller

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -14,21 +14,27 @@ class AuthController extends Controller
 {
     public function auth(Request $request) {
 
+        // rule validate email & password
         $validator = Validator::make($request->all(), [
             'email' => 'required|email',
             'password' => 'required'
         ]);
 
+        // if validator fails
         if($validator->fails()) {
             return response()->json($validator->messages(), 400);
         }
 
+        // if validator success, check request email user
         $user = User::where('email', $request->email)->first();
 
+        // if email user valid, continue process
         if($user) {
 
+            // check request password
             $isValidPassword = Hash::check($request->password, $user->password);
 
+            // if request password, true..
             if($isValidPassword){
 
                 // generate token
@@ -45,12 +51,17 @@ class AuthController extends Controller
 
     private function generateToken($user) {
 
+        // call from env
         $jwtKey = env('JWT_KEY');
         $jwtExpired = env('JWT_EXPIRED');
 
+        // get timestamp ( now )
         $now = now()->timestamp;
+
+        // expired = now + add minutes then get the timestamp
         $expired = now()->addMinutes($jwtExpired)->timestamp;
 
+        // payload
         $payload = [
             'iss' => 'muvi-flex', // issued by
             'iat' => $now, // issued at
@@ -59,8 +70,10 @@ class AuthController extends Controller
             'user' => $user
         ];
 
+        // encode an JWT with payload, $jwtKey, and HS256 type encryption
         $token = JWT::encode($payload, $jwtKey, 'HS256');
 
+        // return
         return $token;
     }
 }
