c4e04022375344922c3598b633d97e7ad14b157e
unknown <bayuxbca@gmail.com>
2023-05-22 13:54:07 +0700

    create: custom Artisan command. Type 'php artisan get:byyy' to find it out!

---
+++ app/Console/Commands/Byyy.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class Byyy extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'get:byyy';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Who's Byyy? Let Find It Out!";
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        return $this->info('Byyy is Handsome People, Who Have a Beauty Girlfriend As Dentits At Special Region Of Yogyakarta');
+    }
+}
